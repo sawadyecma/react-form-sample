b08055bbd37234d165729daf4e94c1c56988f3b4
sawadyecma <snowslidez19m@gmail.com>
2025-05-12 15:28:21 +0900

    feat: form-02

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import { Form_01 } from "./comps/form-01";
+import { Form_02 } from "./comps/form-02";
 import "./style.css";
 
 function App() {
   return (
-    <>
+    <div
+      style={{
+        display: "grid",
+        gap: 12,
+      }}
+    >
       <Form_01 />
-    </>
+      <Form_02 />
+    </div>
   );
 }
 
---
+++ src/comps/form-02.tsx
@@ -0,0 +1,51 @@
+import { useForm, Controller, type SubmitHandler } from "react-hook-form";
+
+interface IFormInputs {
+  isOk: boolean;
+}
+
+export function Form_02() {
+  const { handleSubmit, control, formState, watch } = useForm<IFormInputs>({
+    defaultValues: {
+      isOk: false,
+    },
+  });
+  const onSubmit: SubmitHandler<IFormInputs> = (data) => console.log(data);
+  console.log(watch("isOk")); // you can watch individual input by pass the name of the input
+
+  return (
+    <form className="form" onSubmit={handleSubmit(onSubmit)}>
+      <Controller
+        name="isOk"
+        control={control}
+        rules={{ required: "Please check it!" }}
+        render={({ field }) => (
+          <Checkbox checked={field.value} onChange={field.onChange} />
+        )}
+      />
+      {formState.errors.isOk && (
+        <p className="error-msg">{formState.errors.isOk.message}</p>
+      )}
+      <input type="submit" />
+    </form>
+  );
+}
+
+// eslint-disable-next-line react-refresh/only-export-components
+const Checkbox = ({
+  checked,
+  onChange,
+}: {
+  checked: boolean;
+  onChange: (checked: boolean) => void;
+}) => {
+  return (
+    <input
+      type="checkbox"
+      checked={checked}
+      onChange={(e) => {
+        onChange(e.target.checked);
+      }}
+    />
+  );
+};
